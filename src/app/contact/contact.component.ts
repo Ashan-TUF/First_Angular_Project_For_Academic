6be9350316f47e9d73fa1e667053ce41c68e7329
Ashan Kavindu <93070984+Ashan-TUF@users.noreply.github.com>
2025-01-28 18:35:13 +0530

    First-Angular-Project
    This is my first Angular Project. This project is for academic purpose only.

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+import {AboutComponent} from "../about/about.component";
+
+
+@Component({
+selector:'app-contact',
+  templateUrl:'./contact.component.html',
+  styleUrl:'./contact.component.scss',
+  imports:[AboutComponent],
+  standalone:true
+})
+export class ContactComponent{
+
+}
